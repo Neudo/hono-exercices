a237f9f24c3e13a11171bcf33cd05c93ec94942e
Neudo <78046919+Neudo@users.noreply.github.com>
2021-08-18 12:16:16 +0200

    ex02

---
+++ exercice-02/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ *@package Show social network
+ *@version 1.0.0
+ */
+/*
+Plugin Name: Show social network
+Description : Permet d'afficher une liste de réseaux sociaux via un shortcode. L'utilisateur du wordpress, pourra ensuite importer sa liste depuis le shortcode en question.
+Version : 1.0.0
+*/
+
+
+[show_social_network icon="https://fontawesome.com/v5.15/icons/share-alt?style=solid" icon_color="#333333" class=""]
+<ul>
+  <li>Facebook : <a href="https://facebook.com">'https://facebook.com'</a> </li>
+  <li>Instagram : <a href="https://instagram.com">'https://instagram.com'</a> </li>
+  <li>Linkedin : <a href="https://www.linkedin.com">'https://www.linkedin.com'</a> </li>
+  <li>GitHub : <a href="https://github.com/honoagency">'https://github.com'</a> </li>
+</ul>
+[/show_social_network] 
+
+?>
